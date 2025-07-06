700e34c85d8644844a0e96293e50dd077564769d
rubyshah755 <shahruby755@gmail.com>
2025-07-06 13:55:48 +0545

    mystate

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import MyContext from './myContext'
+import { BiCategory } from 'react-icons/bi';
+import { toast } from 'react-toastify';
+import { addDoc, collection } from 'firebase/firestore';
+import { fireDB } from '../../firebase/FirebaseConfig';
 
 function myState(props) {
     const [mode, setMode] = useState('Light');
@@ -16,6 +20,68 @@ function myState(props) {
     }
 
     const [loading, setLoading] = useState(false);
+
+    const [products, setProducts] = useState({
+      title : null,
+      price : null,
+      imageUrl : null,
+      category : null,
+      description : null,
+      time : new Date().toLocaleString(
+        "en-US",
+        {
+          month: "short",
+          day: "2-digit",
+          year: "numeric",
+        }
+      )
+    });
+    const addProduct = async () => {
+        if(products.title == null || products.price == null || products.imageUrl == null || products.category == null || products.description == null)
+        {
+          return toast.error("all fields are required")
+        }
+        setLoading(true)
+        try{
+            const productRef = collection(fireDB, 'product')
+            await addDoc(productRef, products)
+            toast.success("Add product successfully")
+            setLoading(false)
+        }
+        catch (error) {
+            console.log(error);
+            toast.error(error.message);
+            setLoading(false)
+        }
+        finally{
+          setLoading(false)
+        }
+    }
+    const [product, setProduct] = useState([]);
+    
+        
+        const getProductData = async () => {
+            setLoading(true)
+            try {
+                const q = query(
+                    collection(fireDB, 'products'),
+                    orderBy('time')
+                );
+                const data = onSnapshot(q, (QuerySnapshot) =>{
+                    let productArray = [];
+                    QuerySnapshot.forEach((doc)=>{
+                       productArray.push({...doc.data(), id: doc.id}); 
+                    });
+                    setproduct(productArray)
+                    setLoading(false)
+                });
+                return () => data;
+            }
+            catch {
+                console.log(error)
+                setLoading(false)
+            }
+        }
     
   return (
     <MyContext.Provider value={{mode, toogleMode, loading, setLoading}}>
--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -42,6 +42,7 @@ function Login() {
                 setLoading(false);
             }
     }
+    
     return (
         <div className=' flex justify-center items-center h-screen'>
             {loading && <Loader/>}
