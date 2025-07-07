efc0c4d74ce4b96fa0c3222cf6db1085fa65c038
rubyshah755 <shahruby755@gmail.com>
2025-07-07 14:12:44 +0545

    addproduct

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -2,99 +2,142 @@ import { useEffect, useState } from 'react'
 import MyContext from './myContext'
 import { BiCategory } from 'react-icons/bi';
 import { toast } from 'react-toastify';
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, setDoc } from 'firebase/firestore';
 import { fireDB } from '../../firebase/FirebaseConfig';
 
 function myState(props) {
-    const [mode, setMode] = useState('Light');
+  const [mode, setMode] = useState('Light');
 
-    const toogleMode = () =>{
-      if(mode ==='Light'){
-        setMode('dark');
-        document.body.style.backgroundColor = "rgb(17, 24, 39)"
-      }
-      else{
-        setMode('Light');
-        document.body.style.backgroundColor = "white"
-      }
+  const toogleMode = () => {
+    if (mode === 'Light') {
+      setMode('dark');
+      document.body.style.backgroundColor = "rgb(17, 24, 39)"
     }
-
-    const [loading, setLoading] = useState(false);
-
-    const [products, setProducts] = useState({
-      title : null,
-      price : null,
-      imageUrl : null,
-      category : null,
-      description : null,
-      time : new Date().toLocaleString(
-        "en-US",
-        {
-          month: "short",
-          day: "2-digit",
-          year: "numeric",
-        }
-      )
-    });
-    const addProduct = async () => {
-        if(products.title == null || products.price == null || products.imageUrl == null || products.category == null || products.description == null)
-        {
-          return toast.error("all fields are required")
-        }
-        setLoading(true)
-        try{
-            const productRef = collection(fireDB, 'product')
-            await addDoc(productRef, products)
-            toast.success("Add product successfully")
-            setTimeout(() => {
-            window.location.href = '/dashboard'
-          }, 800);
-            getProductData();
-            setLoading(false)
-        }
-        catch (error) {
-            console.log(error);
-            toast.error(error.message);
-            setLoading(false)
-        }
-        finally{
-          setLoading(false)
-        }
+    else {
+      setMode('Light');
+      document.body.style.backgroundColor = "white"
     }
-    const [product, setProduct] = useState([]);
-    
-        
-        const getProductData = async () => {
-            setLoading(true)
-            try {
-                const q = query(
-                    collection(fireDB, 'products'),
-                    orderBy('time')
-                );
-                const data = onSnapshot(q, (QuerySnapshot) =>{
-                    let productArray = [];
-                    QuerySnapshot.forEach((doc)=>{
-                       productArray.push({...doc.data(), id: doc.id}); 
-                    });
-                    setproduct(productArray)
-                    setLoading(false)
-                });
-                return () => data;
-            }
-            catch {
-                console.log(error)
-                setLoading(false)
-            }
-        }
-        useEffect(() => {
-          getProductData();
-        }, []);
-    
+  }
+
+  const [loading, setLoading] = useState(false);
+
+  const [products, setProducts] = useState({
+    title: null,
+    price: null,
+    imageUrl: null,
+    category: null,
+    description: null,
+    time: new Date().toLocaleString(
+      "en-US",
+      {
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+      }
+    )
+  });
+  const addProduct = async () => {
+    if (products.title == null || products.price == null || products.imageUrl == null || products.category == null || products.description == null) {
+      return toast.error("all fields are required")
+    }
+    setLoading(true)
+    try {
+      const productRef = collection(fireDB, 'product')
+      await addDoc(productRef, products)
+      toast.success("Add product successfully")
+      setTimeout(() => {
+        window.location.href = '/dashboard'
+      }, 800);
+      getProductData();
+      setLoading(false)
+    }
+    catch (error) {
+      console.log(error);
+      toast.error(error.message);
+      setLoading(false)
+    }
+    finally {
+      setLoading(false)
+    }
+//set products("")
+
+  }
+  const [product, setProduct] = useState([]);
+
+
+  const getProductData = async () => {
+    setLoading(true)
+    try {
+      const q = query(
+        collection(fireDB, 'products'),
+        orderBy('time')
+      );
+      const data = onSnapshot(q, (QuerySnapshot) => {
+        let productArray = [];
+        QuerySnapshot.forEach((doc) => {
+          productArray.push({ ...doc.data(), id: doc.id });
+        });
+        setproduct(productArray)
+        setLoading(false)
+      });
+      return () => data;
+    }
+    catch {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+  useEffect(() => {
+    getProductData();
+  }, []);
+
+  //update producys functions
+
+  const edithandle = (item)=>{
+    setProducts(item)
+  }
+
+  const updateProduct = async ()=>{
+    setLoading(true)
+
+    try {
+      await setDoc(doc(fireDB, 'products',products.id), products)
+      toast.success("Product Updated Successfully")
+      // window.location.href = '/dashboard'
+      setTimeout(() => )
+      getProductData();
+      setLoading(false)
+
+    } 
+    catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  // delete products
+
+  const deleteProduct = async (item) => {
+    setLoading(true)
+    try {
+      await deleteDoc(doc(fireDB, 'products', item.id))
+      toast.success('Product Deleted Successfully')
+      getProductData();
+      setLoading(false)
+
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
   return (
-    <MyContext.Provider value={{mode, toogleMode, loading, setLoading,
+    <MyContext.Provider value={{
+      mode, toogleMode, loading, setLoading,
       products, setProducts, addProduct, product
+      edithandle, updateProduct, deleteProduct
     }}>
-        {props.children}
+      {props.children}
     </MyContext.Provider >
   )
 }
--- src/pages/admin/dashboard/page/UpdateProduct.jsx
+++ src/pages/admin/dashboard/page/UpdateProduct.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import myContext from '../../../../context/data/myContext';
 
 function UpdateProduct() {
+
+    const context = useContext(myContext);
+    const {products, setProducts, UpdateProduct} = context;
+
     return (
         <div>
             <div className=' flex justify-center items-center h-screen'>
@@ -10,6 +15,8 @@ function UpdateProduct() {
                     </div>
                     <div>
                         <input type="text"
+                        value={products.title}
+                        onChange={(e)=> setProducts({...products, title: e.target.value})}
                             name='title'
                             className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                             placeholder='Product title'
@@ -17,6 +24,8 @@ function UpdateProduct() {
                     </div>
                     <div>
                         <input type="text"
+                        value={products.price}
+                        onChange={(e)=> setProducts({...products, price: e.target.value})}
                             name='price'
                             className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                             placeholder='Product price'
@@ -24,6 +33,8 @@ function UpdateProduct() {
                     </div>
                     <div>
                         <input type="text"
+                        value={products.imageUrl}
+                        onChange={(e)=> setProducts({...products, imageUrl: e.target.value})}
                             name='imageurl'
                             className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                             placeholder='Product imageUrl'
@@ -31,6 +42,8 @@ function UpdateProduct() {
                     </div>
                     <div>
                         <input type="text"
+                        value={products.category}
+                        onChange={(e)=> setProducts({...products, category: e.target.value})}
                             name='category'
                             className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                             placeholder='Product category'
@@ -38,13 +51,15 @@ function UpdateProduct() {
                     </div>
                     <div>
                        <textarea cols="30" rows="10" name='title'
+                       value={products.description}
+                        onChange={(e)=> setProducts({...products, description: e.target.value})}
                             className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
-                            placeholder='Product title'>
+                            placeholder='Product description'>
 
                        </textarea>
                     </div>
                     <div className=' flex justify-center mb-3'>
-                        <button
+                        <button onClick={updateProduct}
                             className=' bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg'>
                             Update Product
                         </button>
