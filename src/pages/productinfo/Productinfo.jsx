412ad08a3a3f5efe80f1fe445b69b5d3406872a5
rubyshah755 <shahruby755@gmail.com>
2025-07-15 14:38:42 +0545

    prod

--- src/pages/productinfo/Productinfo.jsx
+++ src/pages/productinfo/Productinfo.jsx
@@ -1,10 +1,57 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Layout from '../../components/layout/Layout'
 import myContext from '../../context/data/myContext'
-// import Layout from '../../components/layout/Layout'
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { doc, getDoc } from 'firebase/firestore/lite';
+import { toast } from 'react-toastify';
+import { addToCart } from "../../redux/cartSlice";
+import { fireDB } from "../../firebase/FirebaseConfig";
 
 function ProductInfo() {
-    const context = useContext(myContext)
+    const context = useContext(myContext);
+    const {loading, setLoading } = context;
+
+    const [products, setProducts] = useState('')
+    const params = useParams()
+    // console.log(products.title)
+
+    const getProductData = async () => {
+        setLoading(true)
+                try {
+            const productTemp = await getDoc(doc(fireDB, "products", params.id))
+            // console.log(productTemp)
+            setProducts(productTemp.data());
+            // console.log(productTemp.data())
+            setLoading(false)
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
+
+    useEffect(() => {
+        getProductData()
+
+    }, [])
+
+
+
+    const dispatch = useDispatch()
+    const cartItems = useSelector((state) => state.cart)
+    // console.log(cartItems)
+
+    // add to cart
+    const addCart = (products) => {
+        dispatch(addToCart(products))
+        toast.success('add to cart');
+    }
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cartItems));
+    }, [cartItems])
+    
 
     return (
         <Layout>
